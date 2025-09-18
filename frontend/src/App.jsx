ea15bae98e6f6c8ece8a63b9ab14d218df6404d8
mdanassaifi <cscanas533@gmail.com>
2025-09-18 18:21:03 +0530

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
                 <ProtectedRoute>
                   <Dashboard />
                 </ProtectedRoute>
-              }
+              } 
             />
 
             {/* Admin Only */}
